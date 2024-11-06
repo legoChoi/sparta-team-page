98c60f8f74f75606231f2e6367b0c35ca6932b5d
legoChoi <chlx159@gmail.com>
2024-11-06 10:07:49 +0900

    [refactor] FB config 분리 및 로그인, 회원가입 연결

--- home.js
+++ home.js
@@ -1,17 +1,8 @@
-// Import the functions you need from the SDKs you need
+import { firebaseConfig } from './config.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDU_brbheU3hn6U-ydg3RI08a8bNJxYnJI",
-    authDomain: "sparta-team-page.firebaseapp.com",
-    projectId: "sparta-team-page",
-    storageBucket: "sparta-team-page.firebasestorage.app",
-    messagingSenderId: "516820477461",
-    appId: "1:516820477461:web:c5ba890aad01ba8e5a901c"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -22,7 +13,6 @@ const auth = getAuth();
 // GET: /members/photos
 let memberPhotos = await getDocs(collection(db, "members"));
 let photos = [];
-
 
 memberPhotos.forEach((e) => {
   console.log(e.data());
@@ -35,7 +25,7 @@ memberPhotos.forEach((e) => {
 
 let target = photos[0];
 
-// GET: /members/:memberId
+// GET: apis/members/:memberId
 // member 문서 ID로 검색
 let member = await getDoc(doc(db, "members", target.memberId));
 console.log(member.data());
--- join.js
+++ join.js
@@ -1,64 +1,41 @@
+import { firebaseConfig } from './config.js';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { collection, addDoc, getDocs, doc, getDoc, query } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getAuth, setPersistence, createUserWithEmailAndPassword, signInWithEmailAndPassword, browserSessionPersistence } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
- 
-const firebaseConfig = {
-  apiKey: "AIzaSyDU_brbheU3hn6U-ydg3RI08a8bNJxYnJI",
-  authDomain: "sparta-team-page.firebaseapp.com",
-  projectId: "sparta-team-page",
-  storageBucket: "sparta-team-page.firebasestorage.app",
-  messagingSenderId: "516820477461",
-  appId: "1:516820477461:web:c5ba890aad01ba8e5a901c"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 const auth = getAuth();
 
-setPersistence(auth, browserSessionPersistence).then().catch((error) => {
-  // Handle Errors here.
-  const errorCode = error.code;
-  const errorMessage = error.message;
-});
-
-// POST: /users/login
-// 유저 로그인
-document.getElementById('btn_login').addEventListener('click', () => {
-  event.preventDefault();
-  const logInId = document.getElementById('id').value;
-  const logInPw = document.getElementById('password').value;
-
-  signInWithEmailAndPassword(auth, logInId, logInPw)
-    .then((userCredential) => {
-      // Signed in 
-      console.log(userCredential);
-      location.href = "home.html";
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
-});
-
-// POST: /users/join
+// POST: apis/users/join
 // 유저 회원가입
 document.getElementById('btn_join').addEventListener('click', () => {
   const email = document.getElementById('joinId').value;
   const password = document.getElementById('joinPassword').value;
   const nick = document.getElementById('joinNick').value;
 
+  if (email.length <= 0 | password.length <= 0 | nick.length <= 0) {
+    alert('모두 입력해주세요.'); 
+    return;
+  }
+
   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+    .then(async (userCredential) => {
       console.log(userCredential);
       
       // Signed up 
       const user = userCredential.user;
-      // ...
+
+      const newUser = await addDoc(collection(db, 'users'), {
+        id: email,
+        nick,
+      })
+
+      alert('회원가입 성공');
+      location.href = "./login.html";
     })
     .catch((error) => {
       if (email.length <= 0 | password <= 0 | nick <= 0) alert('');
